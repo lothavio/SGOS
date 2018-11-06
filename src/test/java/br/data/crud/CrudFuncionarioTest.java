b623136748d09901c2d0bb57faff6682f3fc88c7
erick <erick_zen_@hotmail.com>
2018-11-06 04:56:56 -0200

    mudanças(maiores) no CrudFuncionarioTest

--- src/test/java/br/data/crud/CrudFuncionarioTest.java
+++ src/test/java/br/data/crud/CrudFuncionarioTest.java
@@ -5,6 +5,10 @@
  */
 package br.data.crud;
 
+import br.data.entity.Funcionario;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.persistence.EntityManager;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -41,28 +45,25 @@ public class CrudFuncionarioTest {
     /**
      * Test of getEntityManager method, of class CrudFuncionario.
      */
-    @Test
-    public void testGetEntityManager() {
-        System.out.println("getEntityManager");
-        CrudFuncionario instance = new CrudFuncionario();
-        EntityManager expResult = null;
-        EntityManager result = instance.getEntityManager();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }
-
     /**
      * Test of logar method, of class CrudFuncionario.
      */
     @Test
-    public void testLogar() {
+    public void testLogar() throws ParseException {
         System.out.println("logar");
-        String email = "";
-        String senha = "";
-        CrudFuncionario instance = new CrudFuncionario();
-        boolean expResult = false;
-        boolean result = instance.logar(email, senha);
+        String email = "teste";
+        String senha = "teste";
+        
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = formato.parse("23/11/2015");
+        Funcionario instance = new Funcionario(2, "teste", "teste", "teste", data);
+        String expResult = "okay";
+        String result;
+        if((instance.getEmail() == email) && (instance.getSenha() == senha)){
+            result = "okay";
+        } else {
+            result = "erro";
+        }
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
