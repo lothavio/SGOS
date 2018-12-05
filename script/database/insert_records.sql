c74cc0ca2fc06c0f674ff213abc1b4a0822ee4c4
erick <erick_zen_@hotmail.com>
2018-12-05 01:15:46 -0200

    script de insert --insert_records

---
+++ script/database/insert_records.sql
@@ -0,0 +1,13 @@
+insert into departamento values (1, 'TI');
+
+insert into funcionario values (1, 'Erick Iwamoto', 'erick@email.com', 'teste', '01-01-2018', 1);
+
+insert into fornecedor values (1, 'dell', 123456789, 'teste');
+insert into fornecedor values (2, 'logitech', 123456789, 'teste');
+insert into fornecedor values (3, 'razer', 123456789, 'teste');
+
+insert into produto values (1, 'mouse', 5, 5.10, 'mouse da dell', 1);
+insert into produto values (2, 'mouse', 2, 10.00, 'mouse da logitech', 2);
+insert into produto values (3, 'mouse', 1, 120.00, 'mouse da razer', 3);
+insert into produto values (4, 'teclado', 5, 6.50, 'teclado da dell', 1);
+insert into produto values (5, 'monitor', 5, 7.00, 'teclado da logitech', 2);
